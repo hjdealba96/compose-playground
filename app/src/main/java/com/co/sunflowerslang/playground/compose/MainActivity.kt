138b388fb4eeb17309ca72d38c518cf61371eaa4
Henry De Alba <hjdealba96@gmail.com>
2022-03-04 20:10:48 -0500

    Added lazy colum sample

--- app/src/main/java/com/co/sunflowerslang/playground/compose/MainActivity.kt
+++ app/src/main/java/com/co/sunflowerslang/playground/compose/MainActivity.kt
@@ -4,20 +4,30 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.co.sunflowerslang.playground.compose.ui.theme.ComposePlaygroundTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +39,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Conversation(messages = dataSet)
                 }
             }
         }
@@ -37,7 +47,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+// How to document or identify every single component just by en eyesight
+
+@Composable
+fun MessageCard(message: Message) {
     Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(id = R.drawable.ic_profile_picture),
@@ -50,17 +71,31 @@ fun Greeting(name: String) {
                 .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         Spacer(modifier = Modifier.width(6.dp))
-        Column {
+        // TODO Study this - remember and mutableStateOf
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor: Color by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
-                text = "Hello $name!",
+                text = message.title,
                 color = MaterialTheme.colors.secondaryVariant,
                 style = MaterialTheme.typography.subtitle1
             )
             Spacer(modifier = Modifier.height(3.dp))
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 2.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 2.dp,
+                color = surfaceColor,
+                modifier = Modifier
+                    // TODO Study this
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
                 Text(
-                    text = "Hey $name!",
-                    style = MaterialTheme.typography.body2
+                    text = message.content,
+                    style = MaterialTheme.typography.body2,
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1
                 )
             }
         }
@@ -83,7 +118,72 @@ fun DefaultPreview() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colors.background
         ) {
-            Greeting("Android")
+            Conversation(messages = dataSet)
         }
     }
 }
+
+private val dataSet by lazy {
+    listOf(
+        Message(
+            title = "Guest",
+            content = "I was trying to reach you!"
+        ),
+        Message(
+            title = "Guest",
+            content = "Please call me back as soon as you can"
+        ),
+        Message(
+            title = "Guest",
+            content = "I really need to talk to you"
+        ),
+        Message(
+            title = "Guest",
+            content = "Hey, take a look at Jetpack Compose, it's great!\n" +
+                    "It's the Android's modern toolkit for building native UI." +
+                    "It simplifies and accelerates UI development on Android." +
+                    "Less code, powerful tools, and intuitive Kotlin APIs :)"
+        ),
+        Message(
+            title = "Guest",
+            content = "See you"
+        ),
+        Message(
+            title = "Guest",
+            content = "Best you the best"
+        ),
+        Message(
+            title = "Guest",
+            content = "Hey, take a look at Jetpack Compose, it's great!\n" +
+                    "It's the Android's modern toolkit for building native UI." +
+                    "It simplifies and accelerates UI development on Android." +
+                    "Less code, powerful tools, and intuitive Kotlin APIs :)"
+        ),
+        Message(
+            title = "Guest",
+            content = "Hey, take a look at Jetpack Compose, it's great!\n" +
+                    "It's the Android's modern toolkit for building native UI." +
+                    "It simplifies and accelerates UI development on Android." +
+                    "Less code, powerful tools, and intuitive Kotlin APIs :)"
+        ),
+        Message(
+            title = "Guest",
+            content = "Hey, take a look at Jetpack Compose, it's great!\n" +
+                    "It's the Android's modern toolkit for building native UI." +
+                    "It simplifies and accelerates UI development on Android." +
+                    "Less code, powerful tools, and intuitive Kotlin APIs :)"
+        ),
+        Message(
+            title = "Guest",
+            content = "Hey, take a look at Jetpack Compose, it's great!\n" +
+                    "It's the Android's modern toolkit for building native UI." +
+                    "It simplifies and accelerates UI development on Android." +
+                    "Less code, powerful tools, and intuitive Kotlin APIs :)"
+        )
+    )
+}
+
+data class Message(
+    val title: String,
+    val content: String
+)
